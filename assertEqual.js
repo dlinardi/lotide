9fee5c60f654e1b61333d0eee524a73c70ed416a
dlinardi <dlinardi98@gmail.com>
2020-11-02 21:44:44 +0000

    refactored head.js and assertEqual.js and implemented tester files using modules

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,4 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(51, 1);
+module.exports = assertEqual;
