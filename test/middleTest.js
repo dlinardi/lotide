f0afae7ab54b9cb4ada903d3c81aad13465a0cea
dlinardi <dlinardi98@gmail.com>
2020-11-03 00:23:09 +0000

    refactored middleTest.js using mocha & chai testing/assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1,2,3,4,5]), [3]); // pass
-assertArraysEqual(middle([1,2,3,4]), [2,3]); // pass
-assertArraysEqual(middle(['dave','carol','frank']), []); // fail
-assertArraysEqual(middle(['this','is','a','test']), ['is','a']); // pass
+describe('#middle', () => {
+
+  it('should return [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it('should return [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+  it("should return ['carol'] for ['dave','carol','frank']", () => {
+    assert.deepEqual(middle(['dave','carol','frank']), ['carol']);
+  });
+
+  it("should return ['is', 'a'] for ['this','is','a','test']", () => {
+    assert.deepEqual(middle(['this','is','a','test']), ['is','a']);
+  });
+
+});
