44f4ed9017ec010450af174d0af4737955c92e05
dlinardi <dlinardi98@gmail.com>
2020-11-03 00:17:10 +0000

    refactored tailTest.js using mocha & chai testing/assertions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe('#tail', () => {
 
+  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-result = tail([1,2,3,4,5,6,7,8]);
-assertEqual(result.length, 7); // ensure we get back 7 elements
-assertEqual(result[0], 2); // ensure first element is 2
-assertEqual(result[6], 8); // ensure seventh element is 8
+  it('should return [2,3,4,5,6,7,8] for [1,2,3,4,5,6,7,8]', () => {
+    assert.deepEqual(tail([1,2,3,4,5,6,7,8]), [2,3,4,5,6,7,8]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+});
